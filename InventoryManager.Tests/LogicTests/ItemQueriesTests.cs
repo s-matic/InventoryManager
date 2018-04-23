b26d2c1401e31deca50f0a2ea7dbe509d1fefb35
Stefan Matic <stefan.matic@me.com>
2018-04-23 21:12:54 +0200

    Add ItemQueriesTests

---
+++ InventoryManager.Tests/LogicTests/ItemQueriesTests.cs
@@ -0,0 +1,27 @@
+﻿using InventoryManager.Domain.Models;
+using InventoryManager.Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManager.Tests.LogicTests
+{
+    [TestClass]
+    public class ItemQueriesTests
+    {
+        [TestMethod]
+        public void Should_return_empty_list_of_items()
+        {
+            //Arrange
+            var itemQueries = new ItemQueries();
+            var expected = new List<Item>();
+
+            //Act
+            var actual = itemQueries.GetItemsAsync();
+            
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

81b16b5e5e15546a43bc4ec32feb26b9d8bb6ba1
Stefan Matic <stefan.matic@me.com>
2018-04-23 21:50:19 +0200

    Add ItemCommands test class

---
+++ InventoryManager.Tests/LogicTests/ItemCommandsTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManager.Tests.LogicTests
+{
+    [TestClass]
+    public class ItemCommandsTests
+    {
+
+    }
+}

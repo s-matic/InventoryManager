42b44ba9c4b24da52d2df76da81f743febbc051e
Stefan Matic <stefan.matic@me.com>
2018-04-23 21:49:09 +0200

    Add tests for query functionality

--- InventoryManager.Tests/LogicTests/ItemQueriesTests.cs
+++ InventoryManager.Tests/LogicTests/ItemQueriesTests.cs
@@ -1,9 +1,11 @@
-﻿using InventoryManager.Domain.Models;
+﻿using InventoryManager.DataAccess;
+using InventoryManager.Domain.Models;
 using InventoryManager.Logic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace InventoryManager.Tests.LogicTests
 {
@@ -11,17 +13,39 @@ namespace InventoryManager.Tests.LogicTests
     public class ItemQueriesTests
     {
         [TestMethod]
-        public void Should_return_empty_list_of_items()
+        public async Task Should_return_empty_list_of_items()
         {
             //Arrange
-            var itemQueries = new ItemQueries();
-            var expected = new List<Item>();
+            var items = new List<Item>(); //This breaks the definition of Unit test, refactor to use Moq library 
+            var itemDataAccess = new ItemDataAccess(items); 
+            var itemQueries = new ItemQueries(itemDataAccess);
 
             //Act
-            var actual = itemQueries.GetItemsAsync();
+            var actual = (List<Item>) await itemQueries.GetItemsAsync();
             
             //Assert
-            Assert.AreEqual(expected, actual);
+            Assert.IsInstanceOfType(actual, typeof(List<Item>));
+        }
+
+        [TestMethod]
+        public async Task Should_return_list_of_items()
+        {
+            //Arrange
+            var items = new List<Item>(); //This breaks the definition of Unit test, refactor to use Moq library 
+            var itemDataAccess = new ItemDataAccess(items);
+            var itemQueries = new ItemQueries(itemDataAccess);
+
+            //Act
+            items.AddRange(new List<Item>
+            {
+                new Item() {Id = Guid.NewGuid()},
+                new Item() {Id = Guid.NewGuid()}
+            });
+            var expected = items;
+            var actual = (List<Item>) await itemQueries.GetItemsAsync();
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
         }
     }
 }
